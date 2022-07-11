6d865e713cc0b62281d7f793695f68307a30d421
JiSolBin <solbin0027@naver.com>
2022-07-11 20:36:12 +0900

    22.07.11 Front
    - @CrossOrigin 추가
    - createProject.html 이용하여 프로젝트 수정 화면 만듦
    - 프로젝트 생성, 수정, 삭제, 나가기 비동기 통신(Ajax)
    - 프로젝트 생성/수정 시 프로젝트 제목 입력 안하면 경고창 띄우도록 수정
    - 기타 css 추가

--- Backend/project/src/main/java/com/flow/project/controller/apicontroller/RoomsApiController.java
+++ Backend/project/src/main/java/com/flow/project/controller/apicontroller/RoomsApiController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
